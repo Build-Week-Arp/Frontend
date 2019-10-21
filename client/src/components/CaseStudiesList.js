b8a40a53327758db4323dd54a5881bed958804f3
andrew15722 <andrew15722@yahoo.com>
2019-10-21 14:15:38 -0700

    added semantic ui to style the case study cards, added some additional styles.

--- client/src/components/CaseStudiesList.js
+++ client/src/components/CaseStudiesList.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import { Card, Icon, Image } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
+// todo may need to come back and refactor the state of this component and add some routes.
 
 const CaseStudiesList = () => {
 	const [ data, setData ] = useState({
@@ -26,6 +30,22 @@ const CaseStudiesList = () => {
 					'https://images.unsplash.com/photo-1506260408121-e353d10b87c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80',
 				likes: 9,
 				views: 20
+			},
+			{
+				title: 'Hills',
+				author: 'by JP Eliares',
+				imgURL:
+					'https://images.unsplash.com/photo-1506260408121-e353d10b87c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80',
+				likes: 9,
+				views: 20
+			},
+			{
+				title: 'Hills',
+				author: 'by JP Eliares',
+				imgURL:
+					'https://images.unsplash.com/photo-1506260408121-e353d10b87c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80',
+				likes: 9,
+				views: 20
 			}
 		]
 	});
@@ -34,36 +54,56 @@ const CaseStudiesList = () => {
 		<div>
 			<nav className="case-nav">
 				<div className="case-btn-container">
-					<button className="case-btn">Upload</button>
-					<button className="case-btn">Message</button>
+					<button className="case-btn-1">Upload</button>
+					<button className="case-btn-2">Message</button>
 				</div>
-				<a className="case-a" href="#">
-					Case Studies
-				</a>
-				<a className="case-a" href="#">
-					Illustrations
-				</a>
-				<a className="case-a" href="#">
-					Photography
-				</a>
+
+				<span className="middle-link">
+					<a className="case-a" href="#">
+						Case Studies
+					</a>
+					<a className="case-a" href="#">
+						Illustrations
+					</a>
+					<a className="case-a" href="#">
+						Photography
+					</a>
+				</span>
+
 				<span className="case-view-btn">
 					<a href="#">View All </a>
 				</span>
 			</nav>
+			<Card.Group itemsPerRow={5}>
+				{data.caseStudies.map((img) => {
+					console.log(img);
+					return (
+						<Card>
+							<Image src={img.imgURL} wrapped ui={false} />
+							<Card.Content>
+								<Card.Header>{img.title}</Card.Header>
+								<Card.Meta>
+									<span className="date">{img.author}</span>
+								</Card.Meta>
+							</Card.Content>
 
-			{data.caseStudies.map((img) => {
-				console.log(img);
-				return (
-					<div>
-						<img src={img.imgURL} alt="" width="600px" height="300px" />
-						<h3>{img.title}</h3>
-						<p>{img.author}</p>
-						<p>likes: {img.likes}</p>
-						<p> views: {img.views}</p>
-						<button>Edit</button>
-					</div>
-				);
-			})}
+							<Card.Content extra>
+								<span className="heart-icon">
+									<Icon name="heart" />
+									{img.likes}
+								</span>
+
+								<span className="eye-icon">
+									<Icon name="eye" />
+									{img.views}
+								</span>
+
+								<button>Edit</button>
+							</Card.Content>
+						</Card>
+					);
+				})}
+			</Card.Group>
 		</div>
 	);
 };
