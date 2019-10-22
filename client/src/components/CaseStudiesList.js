7d73ce343b2cd79b3d1f10b97002425f580f7efd
andrew15722 <andrew15722@yahoo.com>
2019-10-22 13:50:36 -0700

    have state set up for modal

--- client/src/components/CaseStudiesList.js
+++ client/src/components/CaseStudiesList.js
@@ -3,62 +3,77 @@ import { Card, Icon, Image } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
 const CaseStudiesList = ({ data }) => {
-	return (
-		<div>
-			<nav className="case-nav">
-				<div className="case-btn-container">
-					<button className="case-btn-1">Upload</button>
-					<button className="case-btn-2">Message</button>
-				</div>
+	const [ isUploading, setIsUploading ] = useState(false);
 
-				<span className="middle-link">
-					<a className="case-a" href="#">
-						Case Studies
-					</a>
-					<a className="case-a" href="#">
-						Illustrations
-					</a>
-					<a className="case-a" href="#">
-						Photography
-					</a>
-				</span>
+	// todo add in some dummy data to stand in for components that we are going to integrate
 
-				<span className="case-view-btn">
-					<a href="#">View All </a>
-				</span>
-			</nav>
+	const handleClick = () => {
+		setIsUploading(!isUploading);
+	};
 
-			<Card.Group itemsPerRow={5}>
-				{data.caseStudies.map((img) => {
-					return (
-						<Card>
-							<Image src={img.imgURL} wrapped ui={false} />
-							<Card.Content>
-								<Card.Header>{img.title}</Card.Header>
-								<Card.Meta>
-									<span className="date">{img.author}</span>
-								</Card.Meta>
-							</Card.Content>
+	if (isUploading) {
+		// todo going to make a modal component will will go here.
+		return <button onClick={handleClick} className="case-btn-1" />;
+	} else {
+		return (
+			<div>
+				<nav className="case-nav">
+					<div className="case-btn-container">
+						<button onClick={handleClick} className="case-btn-1">
+							Upload
+						</button>
+						<button className="case-btn-2">Message</button>
+					</div>
 
-							<Card.Content extra>
-								<span className="heart-icon">
-									<Icon name="heart" />
-									{img.likes}
-								</span>
+					<span className="middle-link">
+						<a className="case-a" href="#">
+							Case Studies
+						</a>
+						<a className="case-a" href="#">
+							Illustrations
+						</a>
+						<a className="case-a" href="#">
+							Photography
+						</a>
+					</span>
 
-								<span className="eye-icon">
-									<Icon name="eye" />
-									{img.views}
-								</span>
+					<span className="case-view-btn">
+						<a href="#">View All </a>
+					</span>
+				</nav>
 
-								<button>Edit</button>
-							</Card.Content>
-						</Card>
-					);
-				})}
-			</Card.Group>
-		</div>
-	);
+				<Card.Group itemsPerRow={5}>
+					{data.caseStudies.map((img) => {
+						return (
+							<Card>
+								<Image src={img.imgURL} wrapped ui={false} />
+								<Card.Content>
+									<Card.Header>{img.title}</Card.Header>
+									<Card.Meta>
+										<span className="date">{img.author}</span>
+									</Card.Meta>
+								</Card.Content>
+
+								<Card.Content extra>
+									<span className="heart-icon">
+										<Icon name="heart" />
+										{img.likes}
+									</span>
+
+									<span className="eye-icon">
+										<Icon name="eye" />
+										{img.views}
+									</span>
+
+									<button>Edit</button>
+								</Card.Content>
+							</Card>
+						);
+					})}
+				</Card.Group>
+			</div>
+		);
+	}
 };
 
 export default CaseStudiesList;
