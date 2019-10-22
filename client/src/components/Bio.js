64ad4f4d3098236d60ccef3ec65de2afd1f51240
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-10-22 11:36:26 -0400

    Tweaked info in Bio.js

--- client/src/components/Bio.js
+++ client/src/components/Bio.js
@@ -2,23 +2,42 @@ import React from "react";
 import { Card, Icon, Image } from "semantic-ui-react";
 import styled from "styled-components";
 
+const BioCardContainer = styled.section`
+position: absolute;
+left: 0%;
+`;
+
+const ImageContainer = styled.div`
+ 
+height: 214px;
+width: 100%;
+`;
+
 const SocialIcons = styled.div`
   text-align: center;
 `;
 
 const BioCard = () => (
+  <BioCardContainer className = 'bio-card'>
   <Card>
-    
+    <ImageContainer>
       <Image
-        src="https://react.semantic-ui.com/images/avatar/small/matthew.png"
-        wrapped
-        ui={false}
+        src="https://media.licdn.com/dms/image/C4D03AQGEyqn3rIlCRw/profile-displayphoto-shrink_200_200/0?e=1577318400&v=beta&t=zxpYoLRTOpEFal2OLx1lVshjXP5Tc2txps2DVGOXmFg"
+        avatar
       />
+
+      <p>Leighton Fritze</p>
+      <small>Full Stack Developer</small>
+      <div className="followers">
+        <small>Followers 317 </small> 
+        <small>Following 8</small>
+      </div>
+    </ImageContainer>
+
     <Card.Content>
       <button className="ui button">Follow</button>
       <Card.Header>About Me</Card.Header>
       <Card.Meta>
-        {/* <span className='date'>Joined in 2015</span> */}
       </Card.Meta>
       <Card.Description>
         <p>
@@ -54,6 +73,12 @@ const BioCard = () => (
       </SocialIcons>
     </Card.Content>
   </Card>
+  </BioCardContainer>
 );
 
 export default BioCard;
+
+// facebook official
+//linkedin
+//twitter square
+// mail
