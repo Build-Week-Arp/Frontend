13966f60f2ea9a30210965b3304b093f21e25148
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-10-22 11:49:42 -0400

    started profile page - renders DropDownMenu and BioCard"

---
+++ client/src/components/ProfilePage.js
@@ -0,0 +1,15 @@
+import React from  'react';
+// import CaseStudiesList from './CaseStudiesList';
+import BioCard from './Bio';
+import DropDownMenu from './Menu'
+
+const ProfilePage = () => {
+    return (
+        <div>
+        <DropDownMenu />
+        <BioCard />
+        </div>
+    )
+}
+
+export default ProfilePage;
