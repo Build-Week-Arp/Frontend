b28dec81c5e2cd99d17af177592b9cedf91ca906
michaelmcguiness <michaelmcguiness123@gmail.com>
2019-10-23 16:40:09 -0400

    netlify adjustments

---
+++ src/components/ProfilePage.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+
+import { fetchArtist } from '../actions';
+import CaseStudiesList from './CaseStudiesList';
+import BioCard from './Bio';
+import NavBar from './NavBar';
+
+const ProfilePage = (props) => {
+	return (
+    <>
+		<NavBar />
+		<BioCard />
+		{<CaseStudiesList data={props.artist} />}
+   </>
+	);
+};
+
+const mapStateToProps = (state) => {
+	return({
+		artist: state.artist,
+		isFetching: state.isFetching,
+		isPosting: state.isPosting,
+		error: state.error
+	})
+}
+
+
+export default connect(
+	mapStateToProps,
+	{ fetchArtist }
+ )(ProfilePage);
