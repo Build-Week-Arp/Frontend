1cd563dfbaff9b67cba3569e011fa620c3496a4c
Norma Bunton <te_nj25@hotmail.com>
2019-10-23 13:48:52 -0400

    large and small casestudy componenets

---
+++ client/src/components/LargeCaseStudy.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+const LargeCaseStudy = ( data, setData ) useState => {
+        return 
+            <Container>
+                <Image src={data.imgURL}/>
+                <h1>{data.author}</h1>
+                <h2>author</h2>
+                <h2>{data.likes}</h2>
+                <Title><span/></Title>
+                <Description><span/></Description>
+                <button>Cancel</button>
+                <button>Update Post</button>
+            </Container>
+}
+export default LargeCaseStudy;
