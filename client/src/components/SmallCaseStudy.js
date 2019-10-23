29ad4ebcb399e78d4e56e57f07767ad5821fa3e7
Norma Bunton <te_nj25@hotmail.com>
2019-10-23 14:38:31 -0400

    update2 lg and sm case study

--- client/src/components/SmallCaseStudy.js
+++ client/src/components/SmallCaseStudy.js
@@ -1,12 +1,15 @@
 import React, {useState} from 'react';
 
-const SmallCaseStudy = ( data, setData ) useState => {
-        return 
+const SmallCaseStudy = ( {data, setData} )  => {
+    const [data setData] = useState();
+
+        return {
             <Container>
                 <Image src={data.imgURL}/>
-                <h2>author</h2>
+                <h2>{data.author}</h2>
                 <h2>{data.likes}</h2>
                 <h2>{data.views}</h2>
+                <button>Edit</button>
             </Container>
-}
+};
 export default SmallCaseStudy;
