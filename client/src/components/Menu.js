4d6b1bdcc3724bda9e3de1f8aa00e8eb95eb0f5a
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-10-23 13:50:15 -0400

    added icons for bio and Menu

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,17 +1,82 @@
-import React from 'react'
-import { Dropdown, Menu } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import styled from "styled-components";
+import { FaUserCircle } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
 
-const options = [
-  { key: 1, text: 'Profile', value: 1 },
-  { key: 2, text: 'Messages', value: 2 },
-  { key: 3, text: 'Settings', value: 3 },
-  { key: 4, text: 'Log Out', value: 4 },
-]
+const NavMenu = styled.nav`
+  text-align:center;
+  display: flex;
+  flex-direction: column;
+  width: 10%;
+  background: #191512;
+`;
 
-const DropDownMenu = () => (
-  <Menu compact>
-    <Dropdown icon="user circle icon" options={options} simple item />
-  </Menu>
-)
 
-export default DropDownMenu;
+class BioCard extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      showMenu: false,
+    };
+    
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+  
+  showMenu(event) {
+    event.preventDefault();
+    
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+  
+  closeMenu(event) {
+    
+    if (!this.dropdownMenu.contains(event.target)) {
+      
+      this.setState({ showMenu: false }, () => {
+        document.removeEventListener('click', this.closeMenu);
+      });  
+      
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <FaUserCircle onClick={this.showMenu}>
+          Show menu
+        </FaUserCircle>
+        
+        {
+          this.state.showMenu
+            ? (
+              <div
+                className="menu"
+                ref={(element) => {
+                  this.dropdownMenu = element;
+                }}
+              >
+                <NavMenu>
+                <NavLink className="navLinks" to="#"> Profile </NavLink>
+                <NavLink className="navLinks" to="#"> Messages </NavLink>
+                <NavLink className="navLinks" to="#"> Settings </NavLink>
+                <NavLink className="navLinks" to="#"> Log Out </NavLink>
+                </NavMenu>
+                
+              </div>
+            )
+            : (
+              null
+            )
+        }
+      </div>
+    );
+  }
+}
+
+export default BioCard;
+
+
