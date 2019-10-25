f5b2e3d54602b818ccc8211ca04d939920d787a2
andrew15722 <andrew15722@yahoo.com>
2019-10-25 07:21:06 -0700

    add an add case study function and got it working.

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,138 +1,143 @@
-import {
-    START_FETCHING,
-    FETCH_SUCCESS,
-    FETCH_FAIL,
-    UPDATE_CASE
-} from '../actions';
+import { START_FETCHING, FETCH_SUCCESS, FETCH_FAIL, UPDATE_CASE, ADD_CASE } from '../actions';
 
 import caseStudyImg1 from '../images/caseStudyImg1.jpg';
 import caseStudyImg2 from '../images/caseStudyImg2.png';
 
 const initialState = {
-    isFetching: false,
-    isPosting: false,
-    error: '',
-    artist: {
-        username: "JP Eliares",
-        email: "jpe@gmail.com",
-        firstName: "Jan Patrick",
-        lastName: "Eliares",
-        password: "password",
-        bio: "lorem ipsum",
-        followers: 268,
-        following: 8,
-        title: "UX/UI Designer",
-        caseStudies: 
-            [
-                {
-                    id: 1,
-                    title: "Abstract",
-                    description: "description",
-                    imgURL: caseStudyImg1,
-                    likes: 94,
-                    views: 1784
-                },
-                {
-                    id: 2,
-                    title: "Woman",
-                    description: "description",
-                    imgURL: caseStudyImg2,
-                    likes: 94,
-                    views: 1784
-                },
-                {
-                    id: 3,
-                    title: "Abstract",
-                    description: "description",
-                    imgURL: caseStudyImg1,
-                    likes: 94,
-                    views: 1784
-                },
-                {
-                    id: 4,
-                    title: "Woman",
-                    description: "description",
-                    imgURL: caseStudyImg2,
-                    likes: 94,
-                    views: 1784
-                },
-                {
-                    id: 5,
-                    title: "Abstract",
-                    description: "description",
-                    imgURL: caseStudyImg1,
-                    likes: 94,
-                    views: 1784
-                },
-                {
-                    id: 6,
-                    title: "Woman",
-                    description: "description",
-                    imgURL: caseStudyImg2,
-                    likes: 94,
-                    views: 1784
-                },
-                {
-                    id: 7,
-                    title: "Abstract",
-                    description: "description",
-                    imgURL: caseStudyImg1,
-                    likes: 94,
-                    views: 1784
-                },
-                {
-                    id: 8,
-                    title: "Woman",
-                    description: "description",
-                    imgURL: caseStudyImg2,
-                    likes: 94,
-                    views: 1784
-                } 
-            ]
-    }
-}
+	isFetching: false,
+	isPosting: false,
+	error: '',
+	artist: {
+		username: 'JP Eliares',
+		email: 'jpe@gmail.com',
+		firstName: 'Jan Patrick',
+		lastName: 'Eliares',
+		password: 'password',
+		bio: 'lorem ipsum',
+		followers: 268,
+		following: 8,
+		title: 'UX/UI Designer',
+		caseStudies: [
+			{
+				id: 1,
+				title: 'Abstract',
+				description: 'description',
+				imgURL: caseStudyImg1,
+				likes: 94,
+				views: 1784
+			},
+			{
+				id: 2,
+				title: 'Woman',
+				description: 'description',
+				imgURL: caseStudyImg2,
+				likes: 94,
+				views: 1784
+			},
+			{
+				id: 3,
+				title: 'Abstract',
+				description: 'description',
+				imgURL: caseStudyImg1,
+				likes: 94,
+				views: 1784
+			},
+			{
+				id: 4,
+				title: 'Woman',
+				description: 'description',
+				imgURL: caseStudyImg2,
+				likes: 94,
+				views: 1784
+			},
+			{
+				id: 5,
+				title: 'Abstract',
+				description: 'description',
+				imgURL: caseStudyImg1,
+				likes: 94,
+				views: 1784
+			},
+			{
+				id: 6,
+				title: 'Woman',
+				description: 'description',
+				imgURL: caseStudyImg2,
+				likes: 94,
+				views: 1784
+			},
+			{
+				id: 7,
+				title: 'Abstract',
+				description: 'description',
+				imgURL: caseStudyImg1,
+				likes: 94,
+				views: 1784
+			},
+			{
+				id: 8,
+				title: 'Woman',
+				description: 'description',
+				imgURL: caseStudyImg2,
+				likes: 94,
+				views: 1784
+			}
+		]
+	}
+};
 
 const reducer = (state = initialState, action) => {
-    switch(action.type) {
-        case START_FETCHING:
-            return {
-                ...state,
-                isFetching: true,
-                error: ''
-            }
-        case FETCH_SUCCESS:
-            return {
-                ...state,
-                artist: action.payload,
-                isFetching: false,
-                error: '',
-            }
-        case FETCH_FAIL:
-            return {
-                ...state,
-                isFetching: false,
-                error: action.payload
-            }
-        case UPDATE_CASE:
-            // create new array of case studies w/o case study being updated
-            const newCaseArr = state.artist.caseStudies.map(caseStudy => {
-                if (caseStudy.id === action.payload.id) {
-                    return action.payload.caseStudy
-                } else {
-                    return caseStudy;
-                }
-            })
-            return {
-                ...state,
-                artist: {
-                    ...state.artist,
-                    caseStudies: newCaseArr
-                }
-            }
-        default:
-            console.log("hello");
-            return state;
-    }
-}
+	switch (action.type) {
+		case START_FETCHING:
+			return {
+				...state,
+				isFetching: true,
+				error: ''
+			};
+		case FETCH_SUCCESS:
+			return {
+				...state,
+				artist: action.payload,
+				isFetching: false,
+				error: ''
+			};
+		case FETCH_FAIL:
+			return {
+				...state,
+				isFetching: false,
+				error: action.payload
+			};
+		case UPDATE_CASE:
+			// create new array of case studies w/o case study being updated
+			const newCaseArr = state.artist.caseStudies.map((caseStudy) => {
+				if (caseStudy.id === action.payload.id) {
+					return action.payload.caseStudy;
+				} else {
+					return caseStudy;
+				}
+			});
+
+			return {
+				...state,
+				artist: {
+					...state.artist,
+					caseStudies: newCaseArr
+				}
+			};
+
+		case ADD_CASE:
+			return {
+				...state,
+				artist: {
+					...state.artist,
+					caseStudies: [ ...state.artist.caseStudies, action.payload ]
+				}
+			};
+
+		default:
+			console.log('hello');
+			return state;
+	}
+};
 
 export default reducer;
