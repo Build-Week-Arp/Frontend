79b7c137fc73137e07ff887c1059e5a032763b8a
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-10-24 20:02:30 -0400

    updated form and styled in EditModal.js

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -4,34 +4,53 @@ import { updateCaseStudy } from '../actions';
 import caseStudyImg1 from '../images/caseStudyImg1.jpg';
 import styled from 'styled-components';
 
+const LargeForm = styled.form`
+    border: 1px solid black;
+    display: block;
+    margin-top: 5%;
+    margin-left: auto;
+    margin-right: auto;
+    width: 40%;
+    border-radius: 25px;
+`;
+
 const ArtImage = styled.img`
 display: block;
-margin-top: 5%;
+margin-top: 1%;
 margin-left: auto;
 margin-right: auto;
-width: 50%;
+width: 98%;
+border-radius: 25px;
 `;
 
 const InputForm = styled.input`
     display: flex;
     flex-direction: column;
     margin-left: auto;
-    margin-right: auto;
+    margin-right: 2%;
     margin-top: 1%;
     width: 50%;
-    height: 100px;
+    height: 50px;
     border: none;
     border-bottom: 2px solid gray;
     resize: vertical;
+    border-radius: 5px;
+    padding-left: 2%;
 `;
 
 const SubmitButton = styled.button`
     display: flex;
-    flex-direction: column;    
+    flex-direction: column;  
+    color: white;  
     margin-left: auto;
     margin-right: auto;
     margin-top: 2%;
     margin-bottom: 2%;
+    background: #d12554;
+    border: 2px solid #d12554;
+    border-radius: 15px;
+    
+	
 `;
 
 const EditModal = (props) => {
@@ -51,16 +70,21 @@ const EditModal = (props) => {
 
     }
     return (
-        <form>
+        <LargeForm>
             <ArtImage src={newCase.imgURL} />
             <InputForm
                 name="title" 
                 type="text" 
-                value={newCase.title}
+                placeholder='Enter new title'
                 onChange={handleChange}
             />
-            <SubmitButton onClick={updateIt}>Submit Changes</SubmitButton>
-        </form>
+            <InputForm 
+                name="description" 
+                type="text" 
+                placeholder='Enter new Description'
+                onChange={handleChange}/>
+            <SubmitButton onClick={updateIt}>Update Post</SubmitButton>
+        </LargeForm>
  
     )
 };
@@ -80,3 +104,6 @@ export default connect(
 
 
     // "https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1415&q=80"
+
+  
+    
